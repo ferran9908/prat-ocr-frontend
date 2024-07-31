ca78f67c7834c70afc2603c0891dc41e31ced1c0
Ferran <ferran.9908@gmail.com>
2024-07-31 02:26:14 -0600

    Initial Commit for Forma

--- src/index.js
+++ src/index.js
@@ -1,17 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import Navbar from "./components/Navbar";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Browse from "./pages/Browse";
+import { Toaster } from "react-hot-toast";
+import SampleForm from "./pages/SampleForm";
+// import { ConfigProvider } from 'antd';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/browse",
+    element: <Browse />,
+  },
+  {
+    path: "/browse/:licenseNumber",
+    element: <SampleForm />
+  }
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <ConfigProvider> */}
+    <div className="lg:mx-72 border-r border-l min-h-screen px-2 py-4 bg-slate-50">
+      <Toaster />
+      <Navbar />
+      <RouterProvider router={router} />
+    </div>
+    {/* </ConfigProvider> */}
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
